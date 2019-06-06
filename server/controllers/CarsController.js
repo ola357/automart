f0c54409935bf7a54abd4ee63abb0acbba5edf1d
ola357 <alliolaoluwa@gmail.com>
2019-06-06 14:46:05 +0100

    feature(user): update order amount -update car status [Delivers #166490686] [Delivers #166499311]

--- server/controllers/CarsController.js
+++ server/controllers/CarsController.js
@@ -19,5 +19,31 @@ class CarsController {
     cars.push(car);
     res.send({ status: 200, data: [car] });
   }
+
+  static updateCarAdStatus(req, res) {
+    const car = cars.find(c => c.id === parseInt(req.params.carId, 10));
+    if (!car) {
+      return res.status(404).send({
+        status: 404,
+        error: "The car with the given ID was not found.",
+      });
+    }
+
+    car.status = req.body.status;
+    res.status(200).send({
+      status: 200,
+      data: [
+        {
+          id: car.id,
+          createdOn: Date.now(),
+          manufacturer: car.manufacturer,
+          model: car.model,
+          price: car.price,
+          state: car.state,
+          status: car.status,
+        },
+      ],
+    });
+  }
 }
 export default CarsController;
--- server/controllers/OrdersController.js
+++ server/controllers/OrdersController.js
@@ -1,4 +1,5 @@
 import orders from '../models/orders';
+import cars from '../models/cars';
 import validate from '../validators/validate';
 
 class OrdersController {
@@ -14,6 +15,41 @@ class OrdersController {
     };
     orders.push(order);
     res.send({ status: 200, data: [order] });
+  }
+
+  static updateOrderPrice(req, res) {
+    const order = orders.find(c => c.id === parseInt(req.params.orderId, 10));
+    if (!order) {
+      return res.status(404).send({
+        status: 404,
+        error: "The order with the given ID was not found.",
+      });
+    }
+
+    if ((order.status !== "pending")
+     || (cars[order.carId].status === "sold")) {
+      return res.status(404).send({
+        status: 404,
+        error: "This order is no more available",
+      });
+    }
+    const oldOffer = order.amount;
+    const { error } = validate.updateOrderPrice(req.body);
+    if (error) return res.status(400).send({ status: 400, error: error.details[0].message });
+    order.amount = req.body.amount;
+    const newOffer = order.amount;
+    res.status(200).send({
+      status: 200,
+      data: [
+        {
+          id: order.id,
+          carId: order.carId,
+          status: order.status,
+          oldOffer,
+          newOffer,
+        },
+      ],
+    });
   }
 }
 export default OrdersController;
