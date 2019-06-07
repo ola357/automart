a35df962a76553951ba7a6bc11694aeac01240c8
ola357 <alliolaoluwa@gmail.com>
2019-06-07 21:14:02 +0100

    feature(user): Get route to get cars -and different query parameters -to filter the cars collection -[Delivers #166542033] -[Delivers #166547360] -[Delivers #166547360] -[Delivers #166554421] -[Delivers #166554548] -[Delivers #166554565] -[Delivers #166554598]

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -7,4 +7,5 @@ router.post('/', CarsController.createCarAd);
 router.patch('/:carId/status', CarsController.updateCarAdStatus);
 router.patch('/:carId/price', CarsController.updateCarAdPrice);
 router.get('/:carId', CarsController.getSpecificCar);
+router.get('/', CarsController.getCars);
 export default router;
